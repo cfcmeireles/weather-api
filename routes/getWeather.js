4b2b71bc13832ba8e8af4250d2b2b8cd202b59bd
Carina Meireles <carinacmeireles@gmail.com>
2025-07-27 20:00:24 +0100

    Add server.js and getWeather route

---
+++ routes/getWeather.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const request = require('request');
+
+const getWeather = async (req, res) => {
+    const city = req.params.city;
+    
+    request(`${process.env.BASE_URL}${city}${process.env.API_KEY}`, (error, response, body) => {
+        if (error) {
+            console.error('Request error:', error);
+            return;
+        }
+        try {
+            const data = JSON.parse(body);
+            console.log('Weather data:', data);  
+            res.json(data)
+        } catch (err) {
+            console.error('Error:', err);
+        }
+    });
+}
+
+module.exports = getWeather;
