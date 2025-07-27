dd749b32be82a878bacd81b7ed823803e736635f
Carina Meireles <carinacmeireles@gmail.com>
2025-07-27 22:44:49 +0100

    Trigger build

--- routes/getWeather.js
+++ routes/getWeather.js
@@ -1,21 +1,26 @@
-const request = require('request');
+const request = require("request");
 
 const getWeather = async (req, res) => {
-    const city = req.params.city;
-    
-    request(`${process.env.BASE_URL}${city}${process.env.API_KEY}`, (error, response, body) => {
-        if (error) {
-            console.error('Request error:', error);
-            return;
-        }
-        try {
-            const data = JSON.parse(body);
-            console.log('Weather data:', data);  
-            res.json(data)
-        } catch (err) {
-            console.error('Error:', err);
-        }
-    });
-}
+  const city = req.params.city;
+  const url =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+
+  request(
+    `${url}${city}?key=${process.env.API_KEY}`,
+    (error, response, body) => {
+      if (error) {
+        console.error("Request error:", error);
+        return;
+      }
+      try {
+        const data = JSON.parse(body);
+        console.log("Weather data:", data);
+        res.json(data);
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    }
+  );
+};
 
 module.exports = getWeather;
