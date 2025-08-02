d6f1e17f0591c36ea903ac84a1de1d230f286a80
Carina Meireles <carinacmeireles@gmail.com>
2025-08-02 23:29:27 +0100

    Modify get-weather-route, install redis

---
+++ routes/get-weather-route.js
@@ -0,0 +1,31 @@
+const request = require("request");
+
+const getWeather = (req, res) => {
+  const city = req.params.city;
+  const url =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+
+  request(
+    `${url}${city}?key=${process.env.API_KEY}`,
+    (error, response, body) => {
+      if (error) {
+        console.error("Request error:", error);
+        return;
+      }
+      if (response.statusCode !== 200) {
+        console.error("HTTP error:", response.statusCode);
+        return;
+      }
+      try {
+        const data = JSON.parse(body);
+        res.send(
+          `The current temperature in ${city} is ${data.currentConditions.temp}ºF`
+        );
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    }
+  );
+};
+
+module.exports = getWeather;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const redis = require("redis");
+const port = process.env.PORT;
+const redis_port = process.env.REDIS_PORT;
 const app = express();
-const port = process.env.PORT
+const client = redis.createClient(redis_port);
 
-const getWeather = require('./routes/getWeather');
+const getWeather = require("./routes/get-weather-route");
 
-app.use('/weather/:city', getWeather);
+app.use("/weather/:city", getWeather);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
